0e431a023ccbe9344cd0a020eb4e5c4aba1e811a
IChorne <ichorne@students.kennesaw.edu>
2024-12-09 23:20:29 -0500

    fixed spelling error

--- portfolio-website/src/LandingPage.js
+++ portfolio-website/src/LandingPage.js
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <div className="landing-page">
       <h1>Welcome to My Website!</h1>
-      <p>This is a work in progress, expoect to see my work here soon!</p>
+      <p>This is a work in progress, expect to see my work here soon!</p>
     </div>
   );
 };
